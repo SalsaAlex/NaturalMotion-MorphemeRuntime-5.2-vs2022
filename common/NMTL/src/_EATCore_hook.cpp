36773795c947ba8b30df8ee103c3ba5ce68c36b0
Salsa <81496565+SalsaAlex@users.noreply.github.com>
2026-08-29 03:32:52 -0300

    hook into isLicensed instead of isDevBuild

--- common/NMTL/src/_EATCore_hook.cpp
+++ common/NMTL/src/_EATCore_hook.cpp
@@ -4,16 +4,17 @@
 static HMODULE g_DLLHandle;
 
 //function typedefs
-typedef bool (*pfn_EAT_EnvData_isDevBuildCmd)();
+typedef int (*pfn_EAT_PluginManager_isLicensed)(void* this_, int a2, int a3, int a4, char* a5);
 
 //raw handle to the function inside dll
-pfn_EAT_EnvData_isDevBuildCmd _EAT_EnvData_isDevBuildCmd = nullptr;
+pfn_EAT_PluginManager_isLicensed _EAT_PluginManager_isLicensed = nullptr;
 
 
 //functions
-bool __fastcall isDevBuildCmd(void* this_)
+int __fastcall isLicensed(void* this_, /*because compiler doesnt LISTEN to me*/void* dummy, unsigned int a2, int a3, int a4, bool& a5)
 {
-	return true;
+	a5 = 1;
+	return 4;
 }
 
 
@@ -21,11 +22,11 @@ bool __fastcall isDevBuildCmd(void* this_)
 //now actually do the hooks
 BEGIN_DEFINE_HOOKFUNC(EATCore)
 
-	//get the functions,
-	_EAT_EnvData_isDevBuildCmd = (pfn_EAT_EnvData_isDevBuildCmd)GetProcAddress(g_DLLHandle, "?isDevBuildCmd@EnvData@EAT@@IAE_NXZ");
-	
-	//and hook them
-    HookFunction(_EAT_EnvData_isDevBuildCmd, isDevBuildCmd);
+//get the functions,
+_EAT_PluginManager_isLicensed = (pfn_EAT_PluginManager_isLicensed)GetProcAddress(g_DLLHandle, "?isLicensed@PluginManager@EAT@@QBE?AW4PluginState@12@IHW4LicencePurposes@LicencePlugin@2@AA_N@Z");
+
+//and hook them
+HookFunction(_EAT_PluginManager_isLicensed, isLicensed);
 
 
 END_DEFINE_HOOKFUNC()
